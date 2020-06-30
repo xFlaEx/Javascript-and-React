3e16f435e7a7bed0300623283de7137c3d627b5b
xFlaEx <eduardo568688@gmail.com>
2020-06-30 18:35:11 -0300

    Oper Arrays

--- Javascript ES6/main.js
+++ Javascript ES6/main.js
@@ -1,4 +1,4 @@
-class List {
+/*class List {
     constructor(){
         
         this.data = [];
@@ -31,4 +31,31 @@ document.getElementById('novoTodo').onclick = function(){
     
     MinhaLista.add("NewTodo");
     MinhaLista.user();
-};
+};*/
+
+const arr = [1,3,4,5,8,9];
+
+const newArr = arr.map(function(item, index) {
+    
+    return item+index;
+});
+
+const sum = arr.reduce(function(total, next) {
+
+    return total+next;
+});
+
+const filter = arr.filter(function(item) {
+
+    return item%2 === 0;
+});
+
+const find = arr.find(function(item) {
+
+    return item === 4;
+});
+
+console.log(newArr);
+console.log(sum);
+console.log(filter);
+console.log(find);
