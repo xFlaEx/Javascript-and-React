bc74638c13dc380cb429a62b8e353940b1abe735
xFlaEx <eduardo568688@gmail.com>
2020-06-29 21:08:55 -0300

    Classes

--- Javascript ES6/main.js
+++ Javascript ES6/main.js
@@ -1,8 +1,19 @@
-class Teste{
-    metodo() {
+class TodoList {
+    constructor() {
 
+        this.todos = [];
     }
-    outro() {
 
+    addTodo() {
+
+        this.todos.push('Novo Todo');
+        console.log(this.todos);
     }
 }
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novoTodo').onclick = function(){
+    
+    MinhaLista.addTodo();
+};
