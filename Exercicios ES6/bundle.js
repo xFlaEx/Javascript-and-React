d9dded4da7077345fbd2595b4742bff479f78915
xFlaEx <eduardo568688@gmail.com>
2020-07-01 20:19:44 -0300

    Exe Desestruturação

--- Exercicios ES6/bundle.js
+++ Exercicios ES6/bundle.js
@@ -1,44 +1,31 @@
 "use strict";
 
-// 3.1
-var arr = [1, 2, 3, 4, 5];
-var newArr = arr.map(function (item) {
-  return item + 10;
-});
-console.log(newArr); // 3.2
-// Dica: Utilize uma constante pra function
+//4.1
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome); // Rocketseat
 
-var usuario = {
+console.log(cidade); // Rio do Sul
+
+console.log(estado); // SC
+//4.2
+
+function mostraInfo(usuario) {
+  var nome = usuario.nome,
+      idade = usuario.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo({
   nome: 'Diego',
   idade: 23
-};
-
-var mostraIdade = function mostraIdade(usuario) {
-  return usuario.idade;
-};
-
-console.log(mostraIdade(usuario)); // 3.3
-// Dica: Utilize uma constante pra function
-
-var nome = "Diego";
-var idade = 23;
-
-var mostraUsuario = function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
-};
-
-console.log(mostraUsuario(nome, idade));
-console.log(mostraUsuario(nome)); // 3.4
-
-var promise = function promise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
-
-console.log(promise());
+}));
